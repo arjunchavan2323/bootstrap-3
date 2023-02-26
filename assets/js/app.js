50bdc01e043226b86f496d9d5b5ed17a521752ff
arjunchavan2323 <120880778+arjunchavan2323@users.noreply.github.com>
2023-02-26 18:04:37 +0530

    bootstrapjjj
    bootstrap@3

---
+++ assets/js/app.js
@@ -0,0 +1,73 @@
+let cl=console.log;
+// let person={
+//     fname:"jhon",
+//     lname:"doe",
+//     fullname:function(){
+//         return`${this.fname} ${this.lname}`
+//     }
+// }
+
+// let getperson=person.fullname();
+// cl(getperson)
+
+
+// let fname1={
+//     fname:"jhon",
+//     doe:"lname",
+//     age:22
+// }
+
+// let={fname, doe, age}=fname1
+// cl(fname, doe, age)
+
+
+// let person={
+//     fname:"jhon",
+//     lname:"doe",
+//     age:22
+// }
+
+// let person1={
+//     ...person.fname=arjun
+// }
+// cl(person1)
+
+// ages=[10,20,30,40,50,60,70,80]
+
+// // ages[1]=45;
+// cl(ages)
+
+  
+// let result='';
+// for (let i= ages.length-1; i > 0 ; i--){
+// result+=ages[i]
+// }
+// cl(result)
+
+// function add(a,s){
+// cl(a+s)
+// }
+// add(10,20)
+
+
+// function scope
+// function add(){
+//     var x=10;
+//     var y=20
+//     cl(x)
+
+// }
+// add()
+
+// global scope
+// var x=10
+// cl(x)
+
+// function add(){
+//     let x=10;
+//     let y=20;
+//     cl(x)
+//     cl(y)
+// }
+// add()
+
